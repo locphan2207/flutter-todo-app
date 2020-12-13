4a4fa0f5025282b12febf7321ccffe3947e0bc56
Tan Loc Phan <locphan2207@gmail.com>
2020-12-12 19:54:36 -0800

    Add touch reaction

--- lib/drawer/menu_option.dart
+++ lib/drawer/menu_option.dart
@@ -1,27 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_app/constants.dart';
 
-class MenuOption extends StatelessWidget {
+class MenuOption extends StatefulWidget {
   final String text;
   final IconData icon;
 
   MenuOption({@required this.text, this.icon});
 
   @override
+  _MenuOptionState createState() => _MenuOptionState();
+}
+
+class _MenuOptionState extends State<MenuOption> {
+  bool _isBeingPressed = false;
+
+  @override
   Widget build(BuildContext context) {
-    var itemStyle =
-        Theme.of(context).textTheme.headline5.copyWith(color: MyColor.white);
+    final color = _isBeingPressed ? MyColor.lightGray : MyColor.white;
+    final itemStyle =
+        Theme.of(context).textTheme.headline5.copyWith(color: color);
 
     return GestureDetector(
-      onTap: () => print('h'),
+      onTapDown: handleTap,
+      onTapUp: handleTap,
       child: Padding(
         padding: EdgeInsets.all(MySpacing.small),
         child: Row(children: [
-          Icon(icon, color: MyColor.white, size: 24),
-          SizedBox(width: MySpacing.small),
-          Text(text, style: itemStyle)
+          Icon(widget.icon, color: MyColor.darkGray, size: 26),
+          SizedBox(width: MySpacing.medium),
+          Text(widget.text, style: itemStyle)
         ]),
       ),
     );
+  }
+
+  void handleTap(dynamic tapDetails) {
+    if (tapDetails.runtimeType == TapDownDetails) {
+      setState(() {
+        _isBeingPressed = true;
+      });
+    } else {
+      setState(() {
+        _isBeingPressed = false;
+      });
+    }
   }
 }
